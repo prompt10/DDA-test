282e3ab0889fddd14e4320c8df7a82f3ec89d624
LAPTOP-5K218H50\Supercom - 1 <ritthirach.kongjarit@mail.kmutt.ac.th>
2021-04-24 15:49:29 +0700

    ++Pretty much doing everything by the book except for the float part because using float these days is like telling yourself and everyone you're too indecisive to know what you want to do with all variable you've created.

---
+++ DDA test/DDA test/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace DDA_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, dx, dy, step;
+            string x1s, y1s, x2s, y2s;
+            x1s = Console.ReadLine();
+            y1s = Console.ReadLine();
+            x2s = Console.ReadLine();
+            y2s = Console.ReadLine();
+            double x1 = double.Parse(x1s);
+            double y1 = double.Parse(y1s);
+            double x2 = double.Parse(x2s);
+            double y2 = double.Parse(y2s);
+            dx = x2 - x1;
+            dy = y2 - y1;
+            if (Math.Abs(dx) >= Math.Abs(dy))
+            {
+                step = Math.Abs(dx);
+                dx /= step;
+                dy /= step;
+                x = x1;
+                y = y1;
+                double i;
+                for (i = 1; i <= step; i++)
+                {
+                    Console.WriteLine("{0},{1}", x, y);
+                    x += dx;
+                    y += dy;
+                }
+            }
+            else
+            {
+                step = Math.Abs(dy);
+                dx /= step;
+                dy /= step;
+                x = x1;
+                y = y1;
+                double i;
+                for (i = 1; i <= step; i++)
+                {
+                    Console.WriteLine("{0},{1}", x, y);
+                    x += dx;
+                    y += dy;
+                }
+            }
+            
+
+        }
+    }
+}
